1797501313c25249b3823424e9a3125c78cf13a6
Alban Tiberghien <alban.tiberghien@gmail.com>
2013-05-18 17:40:14 +0200

    Allow to disable leaflet marker clustering

--- geolocation_helper/templates/geolocation_helper/leaflet_fetch_geolocated.js
+++ geolocation_helper/templates/geolocation_helper/leaflet_fetch_geolocated.js
@@ -1,4 +1,7 @@
+//clustering activated = {{using_clustering}}
+{% if using_clustering %}
 var markers = new L.MarkerClusterGroup();
+{% endif %}
 $.get("{% url list_url_name %}", function(data){
 	$.each(data, function(index, value){
 		var marker = new L.Marker(new L.LatLng(value[2], value[1]));
@@ -8,7 +11,13 @@ $.get("{% url list_url_name %}", function(data){
 				marker.bindPopup(data);
 			});
 		});
+		{% if using_clustering %}
 		markers.addLayer(marker);
+		{% else %}
+		marker.addTo(map);
+		{% endif %}
 	});
+	{% if using_clustering %}
 	map.addLayer(markers);
+	{% endif %}
 }, "json");
--- geolocation_helper/templatetags/leaflet_extras.py
+++ geolocation_helper/templatetags/leaflet_extras.py
@@ -3,7 +3,8 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('geolocation_helper/leaflet_fetch_geolocated.js')
-def load_markers(list_url_name, popup_url_name):
+def load_markers(list_url_name, popup_url_name, using_clustering=True):
     return {'list_url_name' : list_url_name,
-            'popup_url_name' : popup_url_name}
+            'popup_url_name' : popup_url_name,
+            'using_clustering' : using_clustering}
     
